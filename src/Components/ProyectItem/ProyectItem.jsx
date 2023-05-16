d82c5a75ebb7b9e734251fd15ebcb746060db33d
Joeyker <oswaldo_avilez@outlook.es>
2023-05-16 19:58:44 -0300

    joey

--- src/Components/ProyectItem/ProyectItem.jsx
+++ src/Components/ProyectItem/ProyectItem.jsx
@@ -15,7 +15,20 @@ const ProyectItem = () => {
     
 
   </div>
-  
+  <div className=" relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-500 rounded-xl p-4 group hover:bg-gradient-to-r from-sky-500 to-sky-100">
+    <img 
+    className=" rounded-xl group-hover:opacity-10"
+    src="img/Cap1.png" 
+    alt="/" />
+    <div className=" hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+      <NavLink to="/proyectport">
+        <h3 className=" text-gray-600 cursor-pointer font-bold uppercase bg-white rounded-lg py-3 px-3 ease-in duration-300 hover:scale-105"> Mas info</h3>
+      </NavLink>
+    </div>
+    
+
+  </div>
+    
   </div>
   )
 }
--- src/pages/proyectport.jsx
+++ src/pages/proyectport.jsx
@@ -6,7 +6,7 @@ const proyectport = () => {
     <div className='py-20'>
     <div className=' max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8'>
         <div className=' col-span-4'>
-        <h1 className=" uppercase text-xl text-sky-700 tracking-widest">Portfolio</h1>
+        <h1 className=" uppercase text-xl text-sky-700 tracking-widest">GifsApp</h1>
          <h1 className=' py-2 text-gray-700 font-bold text-4xl'>Descripcion</h1>
             <p className=' py-4'>Este seria mi sitio web, ofrezco una mejor visualizacion a lo que seria mi formacion como Desarollador. Cree toda la interfaz con react JS y un poco de estilo con Tailwind css</p>
             <Link to="https://github.com/Joeyker-Mayora/portafolio" target="_blank" className=' text-xl font-bold p-3 bg-gradient-to-r from-sky-800 to-sky-300 rounded-xl   text-gray-50 hover:scale-105 ease-in duration-300'>Codigo</Link>
